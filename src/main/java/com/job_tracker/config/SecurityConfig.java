5746f29f0df9f85dc1b94f4c76e0fa259be9db21
SushmaTadiparthi <tadiparthisushma14@gmail.com>
2026-04-30 12:38:56 +0530

    updated SecurityConfig and added CorsConfig

--- src/main/java/com/job_tracker/config/SecurityConfig.java
+++ src/main/java/com/job_tracker/config/SecurityConfig.java
@@ -26,8 +26,8 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.csrf(csrf -> csrf.disable())
-				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+		http.csrf(csrf -> csrf.disable()).cors(cors -> {
+		}).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests(
 						auth -> auth.requestMatchers("/api/auth/**").permitAll().anyRequest().authenticated())
 				.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
